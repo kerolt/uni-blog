588e6b562ea96a922ee4f346a90272a1ec9173aa
liuyx <1517482303@qq.com>
2022-11-25 15:00:31 +0800

    设置开发环境

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src') // 利用@代替src
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const config = loadEnv(mode, './')
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src') // 利用@代替src
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: config.VITE_BASIC_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
